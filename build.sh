f48202774e1c14aa256d98cd6bd8c5643c280232
nukusaba <velixi.vel@gmail.com>
2025-08-07 15:34:56 +0200

    autoremove css map

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ export BUILD_DIR="./build"
 export URN_THEME_PATH="/usr/local/share/urn/themes/$THEME_NAME"
 
 sass src/main.scss $BUILD_DIR/$THEME_NAME.css
+rm $BUILD_DIR/$THEME_NAME.css.map
 sudo mkdir -p $URN_THEME_PATH
 sudo cp $BUILD_DIR/$THEME_NAME.css $URN_THEME_PATH/$THEME_NAME
